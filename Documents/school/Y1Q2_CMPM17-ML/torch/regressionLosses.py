cf380af7ec98d46954d1197cce99bcc0a98b8ffc
Manu Jhawar <153882497+manwhar@users.noreply.github.com>
2026-02-17 16:17:58 -0800

    Delete Documents/school/Y1Q2_CMPM17-ML directory

--- Documents/school/Y1Q2_CMPM17-ML/torch/regressionLosses.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-
-loss_fn = nn.MSELoss()
-
-predicted_values = torch.tensor([1.0, -2.0, 3.0])
-actual_values = torch.tensor([2.0, 3.0, 1.0])
-loss = loss_fn(predicted_values, actual_values)
-print(loss)
